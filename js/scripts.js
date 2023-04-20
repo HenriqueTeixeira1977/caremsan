9cc234ede3e28c4f1e4533a50c82fd5201238a84
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-04-20 11:53:14 -0300

    GetButton-Update200420231153
    Criação do botão do Whatsapp da GetButton,
    implementado em javascript

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,20 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
 	menu.classList.remove('active');
 });
+
+//<!-- GetButton.io widget -->
+//<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+55(13) 98858-9976", // WhatsApp number
+            call_to_action: "Envia-nos uma mensagem", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+            pre_filled_message: "Olá!  venho do seu site, eu gostaria de fazer um orçamento!!!", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+//</script>
+//<!-- /GetButton.io widget -->
